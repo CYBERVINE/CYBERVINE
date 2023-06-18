a1b4faca9181d6f7f41ce4485c225b4f55ddaa6f
CYBERVINE <105403846+CYBERVINE@users.noreply.github.com>
2023-06-17 22:00:36 -0700

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,15 +1,4 @@
 
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-          console.log(data, data.ip)
-    const ipAddress = data.ip;
-    console.log(`IP Address: ${ipAddress}`);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
 console.log('/////////////////////')
 
 fetch('https://ipapi.co/json/') // Replace the URL with the geolocation API endpoint
@@ -23,3 +12,39 @@ fetch('https://ipapi.co/json/') // Replace the URL with the geolocation API endp
   .catch(error => {
     console.error('Error:', error);
   });
+
+console.log('/////////////////////')
+
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+console.log(`Screen Resolution: ${screenWidth}x${screenHeight}`);
+
+console.log('/////////////////////')
+
+fetch('https://api.useragent.io/')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const userAgent = data.user_agent;
+    console.log(`User Agent: ${userAgent}`);
+    // Extract other relevant properties from the data object as needed
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+console.log('/////////////////////')
+
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+          console.log(data, data.ip)
+    const ipAddress = data.ip;
+    console.log(`IP Address: ${ipAddress}`);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
