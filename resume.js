2cdfb14f3bb74a19860a5b26900e5f396535ce19
CYBERVINE <105403846+CYBERVINE@users.noreply.github.com>
2023-06-17 21:49:08 -0700

    Update resume.js

--- resume.js
+++ resume.js
@@ -9,3 +9,17 @@ fetch('https://api.ipify.org?format=json')
   .catch(error => {
     console.error('Error:', error);
   });
+
+console.log('/////////////////////')
+
+fetch('https://ipapi.co/json/') // Replace the URL with the geolocation API endpoint
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Log the entire data object
+    const latitude = data.latitude; // Extract latitude value
+    const longitude = data.longitude; // Extract longitude value
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
