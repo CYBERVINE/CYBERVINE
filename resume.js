afc3898796aae024578a7beb6fe8526aeabc2812
CYBERVINE <105403846+CYBERVINE@users.noreply.github.com>
2023-06-17 20:59:46 -0700

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,14 +1,10 @@
 
-    fetch('https://cybervine.github.io/resume.doc.html')
-        console.log('fetch')
-      .then(response => response.json())
-      console.log('json', response)
-
-      .then(data => {
-        var ipAddress = data.ip;
-        console.log('IP Address:', ipAddress);
-        console.log('data', data)
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipAddress = data.ip;
+    console.log(`IP Address: ${ipAddress}`);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
