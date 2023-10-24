480fcd7b0e6c5ef08f25b59aad7ae224f4496178
Kevin Byrne <kevinspbyrne@gmail.com>
2023-10-23 19:37:40 -0700

    here's the rest

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Here is my resume site and portfolio.
+This is the first project I ever did. It's all one concern ooops!
+It works the way I like it, now that I learned the industry standards thought
+I'd clean up the code to make it more readable and manageable.
+If I ever crack the files open again!
 
--- chess.js
+++ chess.js
@@ -51,164 +51,164 @@ blackCapInc = 350
 // WHITE PIECES HTML //
 
 WLK = document.createElement("img");
-WLK.src = "whiteKnight.png";
+WLK.src = "pieces/whiteKnight.png";
 board.appendChild(WLK)
 WLK.id = "whiteLeftKnight"
 
 WRK = document.createElement("img");
-WRK.src = "whiteKnight.png";
 board.appendChild(WRK)
 WRK.id = "whiteRightKnight"
+WRK.src = "pieces/whiteKnight.png";
 
 WLR = document.createElement("img");
-WLR.src = "whiteRook.png";
 board.appendChild(WLR)
 WLR.id = "whiteLeftRook"
+WLR.src = "pieces/whiteRook.png";
 
 WRR = document.createElement("img");
-WRR.src = "whiteRook.png";
 board.appendChild(WRR)
 WRR.id = "whiteRightRook"
+WRR.src = "pieces/whiteRook.png";
 
 WLB = document.createElement("img");
-WLB.src = "whiteBishop.png";
+WLB.src = "pieces/whiteBishop.png";
 board.appendChild(WLB)
 WLB.id = "whiteLeftBishop"
 
 WRB = document.createElement("img");
-WRB.src = "whiteBishop.png";
+WRB.src = "pieces/whiteBishop.png";
 board.appendChild(WRB)
 WRB.id = "whiteRightBishop"
 
 WQ = document.createElement("img");
-WQ.src = "whiteQueen.png";
+WQ.src = "pieces/whiteQueen.png";
 board.appendChild(WQ)
 WQ.id = "whiteQueen"
 
 WK = document.createElement("img");
-WK.src = "whiteKing.png";
+WK.src = "pieces/whiteKing.png";
 board.appendChild(WK)
 WK.id = "whiteKing"
 
 WP1 = document.createElement("img");
-WP1.src = "whitePawn.png";
+WP1.src = "pieces/whitePawn.png";
 board.appendChild(WP1)
 WP1.id = "whitePawn1"
 
 WP2 = document.createElement("img");
-WP2.src = "whitePawn.png";
+WP2.src = "pieces/whitePawn.png";
 board.appendChild(WP2)
 WP2.id = "whitePawn2"
 
 WP3 = document.createElement("img");
-WP3.src = "whitePawn.png";
+WP3.src = "pieces/whitePawn.png";
 board.appendChild(WP3)
 WP3.id = "whitePawn3"
 
 WP4 = document.createElement("img");
-WP4.src = "whitePawn.png";
+WP4.src = "pieces/whitePawn.png";
 board.appendChild(WP4)
 WP4.id = "whitePawn4"
 
 WP5 = document.createElement("img");
-WP5.src = "whitePawn.png";
+WP5.src = "pieces/whitePawn.png";
 board.appendChild(WP5)
 WP5.id = "whitePawn5"
 
 WP6 = document.createElement("img");
-WP6.src = "whitePawn.png";
+WP6.src = "pieces/whitePawn.png";
 board.appendChild(WP6)
 WP6.id = "whitePawn6"
 
 WP7 = document.createElement("img");
-WP7.src = "whitePawn.png";
+WP7.src = "pieces/whitePawn.png";
 board.appendChild(WP7)
 WP7.id = "whitePawn7"
 
 WP8 = document.createElement("img");
-WP8.src = "whitePawn.png";
+WP8.src = "pieces/whitePawn.png";
 board.appendChild(WP8)
 WP8.id = "whitePawn8"
 
 // BLACK PIECES HTML //
 
 BLB = document.createElement("img");
-BLB.src = "blackBishop.png";
+BLB.src = "pieces/blackBishop.png";
 board.appendChild(BLB)
 BLB.id = "blackLeftBishop"
 
 BRB = document.createElement("img");
-BRB.src = "blackBishop.png";
+BRB.src = "pieces/blackBishop.png";
 board.appendChild(BRB)
 BRB.id = "blackRightBishop"
 
 BLR = document.createElement("img");
-BLR.src = "blackRook.png";
+BLR.src = "pieces/blackRook.png";
 board.appendChild(BLR)
 BLR.id = "blackLeftRook"
 
 BRR = document.createElement("img");
-BRR.src = "blackRook.png";
+BRR.src = "pieces/blackRook.png";
 board.appendChild(BRR)
 BRR.id = "blackRightRook"
 
 BLK = document.createElement("img");
-BLK.src = "blackKnight.png";
+BLK.src = "pieces/blackKnight.png";
 board.appendChild(BLK)
 BLK.id = "blackLeftKnight"
 
 BRK = document.createElement("img");
-BRK.src = "blackKnight.png";
+BRK.src = "pieces/blackKnight.png";
 board.appendChild(BRK)
 BRK.id = "blackRightKnight"
 
 BQ = document.createElement("img");
-BQ.src = "blackQueen.png";
+BQ.src = "pieces/blackQueen.png";
 board.appendChild(BQ)
 BQ.id = "blackQueen"
 
 BK = document.createElement("img");
-BK.src = "blackKing.png";
+BK.src = "pieces/blackKing.png";
 board.appendChild(BK)
 BK.id = "blackKing"
 
 BP1 = document.createElement("img");
-BP1.src = "blackPawn.png";
+BP1.src = "pieces/blackPawn.png";
 board.appendChild(BP1)
 BP1.id = "blackPawn1"
 
 BP2 = document.createElement("img");
-BP2.src = "blackPawn.png";
+BP2.src = "pieces/blackPawn.png";
 board.appendChild(BP2)
 BP2.id = "blackPawn2"
 
 BP3 = document.createElement("img");
-BP3.src = "blackPawn.png";
+BP3.src = "pieces/blackPawn.png";
 board.appendChild(BP3)
 BP3.id = "blackPawn3"
 
 BP4 = document.createElement("img");
-BP4.src = "blackPawn.png";
+BP4.src = "pieces/blackPawn.png";
 board.appendChild(BP4)
 BP4.id = "blackPawn4"
 
 BP5 = document.createElement("img");
-BP5.src = "blackPawn.png";
+BP5.src = "pieces/blackPawn.png";
 board.appendChild(BP5)
 BP5.id = "blackPawn5"
 
 BP6 = document.createElement("img");
-BP6.src = "blackPawn.png";
+BP6.src = "pieces/blackPawn.png";
 board.appendChild(BP6)
 BP6.id = "blackPawn6"
 
 BP7 = document.createElement("img");
-BP7.src = "blackPawn.png";
+BP7.src = "pieces/blackPawn.png";
 board.appendChild(BP7)
 BP7.id = "blackPawn7"
 
 BP8 = document.createElement("img");
-BP8.src = "blackPawn.png";
+BP8.src = "pieces/blackPawn.png";
 board.appendChild(BP8)
 BP8.id = "blackPawn8"
 
@@ -3561,7 +3561,7 @@ findCurrentWhiteAttackPotential()
 
 function makeSound (){
 
-    var audio = new Audio('sound.mp3');
+    var audio = new Audio('pieces/sound.mp3');
     audio.play();
 
 }
